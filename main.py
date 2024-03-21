5c977d0151e144df1bb205a677bb110a3a2e65fa
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-03-21 12:42:22 +0400

    patch request added

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ video_put_args.add_argument("name",type=str,help="Name of the video is required"
 video_put_args.add_argument("views",type=int,help="Views of the video",required = True)
 video_put_args.add_argument("likes",type=int,help="Likes on the video",required = True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name",type=str,help="Name of the video is required")
+video_update_args.add_argument("views",type=int,help="Views of the video")
+video_update_args.add_argument("likes",type=int,help="Likes on the video")
+
 resource_fields = {
     "id":fields.Integer,
     "name" :fields.String,
@@ -46,9 +51,25 @@ class Video(Resource):
         db.session.add(video)
         db.session.commit()
         return video, 201 # creted
-    # def delete(self,video_id):
-    #     del videos[video_id]
-    #     return "",204
+    @marshal_with(resource_fields)
+    def patch(self,video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id = video_id).first()
+        if not result:
+            abort(404,message = "Video doesn't exist, cannot update")
+        if args['name']:
+            result.name = args['name'] 
+        if args['views']:
+            result.views = args['views'] 
+        if args['likes']:
+            result.likes = args['likes'] 
+
+        db.session.commit()
+
+        return result
+    def delete(self,video_id):
+        del videos[video_id]
+        return "",204
 api.add_resource(Video,"/video/<int:video_id>")
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,21 +2,24 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [
-    {"likes":78,"name":"name_1","views":45646226},
-    {"likes":10,"name":"name_2","views":13424234234},
-    {"likes":345,"name":"name_3","views":1034500}
-]
-for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i),data[i])
-    print(response.json())
 
-# input()
-# response = requests.delete(BASE + "video/0")
-# print(response)
-input()
-response = requests.get(BASE + "video/1")
+response = requests.patch(BASE + "video/1",{"views":1234,"likes":34})
 print(response.json())
 
 
 
+# data = [
+#     {"likes":78,"name":"name_1","views":45646226},
+#     {"likes":10,"name":"name_2","views":13424234234},
+#     {"likes":345,"name":"name_3","views":1034500}
+# ]
+# for i in range(len(data)):
+#     response = requests.put(BASE + "video/" + str(i),data[i])
+#     print(response.json())
+
+# # input()
+# # response = requests.delete(BASE + "video/0")
+# # print(response)
+# input()
+# response = requests.get(BASE + "video/1")
+# print(response.json())
