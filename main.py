20c554db640bfc6377070a04a7641eb0881a0fd1
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-03-21 11:22:17 +0400

    delete functionality added

--- main.py
+++ main.py
@@ -16,17 +16,23 @@ videos = {}
 def abort_if_video_id_doesnt_exist(video_id):
     if video_id not in videos:
         abort(404, message = "Video id is not valid...")
-
+def abourt_if_video_already_exists(video_id):
+    if video_id in videos:
+        abort(409, message = "Video already exists with that ID...")
 class HelloWorld(Resource):
     def get(self, video_id):
         abort_if_video_id_doesnt_exist(video_id)
         return videos[video_id]
     def put(self, video_id):
+        abourt_if_video_already_exists(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201 # creted
-        
-api.add_resource(HelloWorld,"/helloworld/<int:video_id>")
+    def delete(self,video_id):
+        abort_if_video_id_doesnt_exist(video_id)
+        del videos[video_id]
+        return "",204
+api.add_resource(HelloWorld,"/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug = True)
--- test.py
+++ test.py
@@ -2,8 +2,21 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-# response = requests.put(BASE + "helloworld/1",{"likes":10,"name":"vid_1","views":1000})
-# print(response.json())
-# input()
-response = requests.get(BASE + "helloworld/1")
+data = [
+    {"likes":78,"name":"name_1","views":45646226},
+    {"likes":10,"name":"name_2","views":13424234234},
+    {"likes":345,"name":"name_3","views":1034500}
+]
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i),data[i])
+    print(response.json())
+
+input()
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/1")
 print(response.json())
+
+
+
